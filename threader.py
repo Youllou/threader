6d5b5bf0b067741f80b0cb1984eabb0fbee24366
youllou <contact@youllou.com>
2025-09-10 16:26:30 -0400

    vibe coding is bad once more

--- threader.py
+++ threader.py
@@ -3,6 +3,25 @@ from slack_sdk.errors import SlackApiError
 from dotenv import load_dotenv
 import os
 
+from sqlalchemy import Column, Integer, String, Boolean, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class ShameConfig(Base):
+    __tablename__ = "shame_config"
+
+    id = Column(Integer, primary_key=True)
+    dest_channel = Column(String, default="wall-of-shame")
+    trigger_emoji = Column(String, default="shame")
+    message_template = Column(
+        String,
+        default=":rotating_light: SHAME! :rotating_light:\n<PERMALINK|this message>"
+    )
+    admin_only = Column(Boolean, default=False)
+
+
 load_dotenv()
 # Initialize app with bot token and signing secret
 app = App(
